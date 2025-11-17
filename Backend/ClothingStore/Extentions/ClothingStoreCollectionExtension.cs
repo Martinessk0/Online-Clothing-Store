0a6e2e7450f82ca160de401aee856f30e2c02dbb
Martin Grahovski <martingrahovski2004@gmail.com>
2025-11-18 00:51:15 +0200

    Add infrastructure Data folder and API extensions

---
+++ Backend/ClothingStore/Extentions/ClothingStoreCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ClothingStoreCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //TODO: uncomment IRepository after adding entityframework
+            //services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
