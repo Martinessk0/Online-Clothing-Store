ef905eca5451499d8fa8fe2e1442942682da353d
Martin Grahovski <martingrahovski2004@gmail.com>
2025-11-19 13:54:51 +0200

    Adding EntityFramework to the project

---
+++ Backend/ClothingStore.Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        //public DbSet<Entity> TableName { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Adding Configuration (seeding)
+            //builder.ApplyConfiguration(new UserConfiguration());
+
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
