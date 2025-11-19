b1c002ac0b3c4a6f1c723b10be034062af1e4bd0
Martin Grahovski <martingrahovski2004@gmail.com>
2025-11-19 13:55:01 +0200

    Adding connection string and user secret

--- Backend/ClothingStore/Program.cs
+++ Backend/ClothingStore/Program.cs
@@ -1,6 +1,15 @@
+using ClothingStore.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var conn = builder.Configuration["Database:Dev"];
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString(conn)));
+//builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+//    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
