4b058d547545cb4294fa4424be08e552d9c05175
Vasil Zunkov <vaskozunkov89@gmail.com>
2025-11-29 00:47:04 +0200

    implemented review, OrderItem, category

---
+++ Vasil/ClothesShop/ClothesShop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothesShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int? ParentID { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
