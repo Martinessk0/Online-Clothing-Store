51f337a1273df5e360545d295dd4eabaab52277c
Didi-Todorova <didi100504@abv.bg>
2025-12-03 22:40:46 +0200

    Add Product Service, Controller and DTO
    Issue #7

--- Backend/ClothingStore.Infrastructure/Data/Entities/Cart.cs
+++ Backend/ClothingStore.Infrastructure/Data/Entities/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace ClothingStore.Infrastructure.Data.Entities
 {
     public class Cart
     {
+        [Key]
         public int Id { get; set; }
+
+
         public string UserId { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
