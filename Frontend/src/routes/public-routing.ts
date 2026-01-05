ef99f7f58e23b50a1049f946f13f8e319c3dd90d
Martin Grahovski <martingrahovski2004@gmail.com>
2026-01-05 15:32:28 +0200

    Adding about page (Closes #76)

--- Frontend/src/routes/public-routing.ts
+++ Frontend/src/routes/public-routing.ts
@@ -11,6 +11,7 @@ import { CartComponent } from '../components/public/cart/cart.component';
 import { CheckoutComponent } from '../components/public/checkout/checkout.component';
 import { MyOrdersComponent } from '../components/public/my-orders/my-orders.component';
 import { OrderDetailsComponent } from '../components/public/order-details/order-details.component';
+import { AboutComponent } from '../components/public/about/about.component';
 
 
 export const publicRoutes: Routes = [
@@ -43,7 +44,7 @@ export const publicRoutes: Routes = [
         path: 'products/:id',
         component: ProductDetailsComponent,
       },
-       {
+      {
         path: 'cart',
         component: CartComponent,
       },
@@ -51,7 +52,11 @@ export const publicRoutes: Routes = [
         path: 'checkout',
         component: CheckoutComponent,
       },
-            {
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
         path: 'orders',
         component: MyOrdersComponent,
         canActivate: [authGuard],
